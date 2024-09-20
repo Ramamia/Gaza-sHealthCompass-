0dd3b3880ea38993a19444f2cc1ab6fc1810490f
rama selwadi <130895629+Ramamia@users.noreply.github.com>
2024-09-21 01:40:58 +0300

    Update CommonNavBar.jsx

--- components/CommonNavBar.jsx
+++ components/CommonNavBar.jsx
@@ -13,32 +13,32 @@ const CommonNavBar = ({ navVisible, handleScrollTo, handleLanguageSwitch }) => {
     return (
         <>
             {navVisible && (
-                <View style={styles.sideNav}>
-                    <TouchableOpacity onPress={() => handleNavigate('HomeScreen')}>
-                        <Text style={styles.navItem}>Home</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleNavigate('Articles')}>
-                        <Text style={styles.navItem}>Articles</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleNavigate('News')}>
-                        <Text style={styles.navItem}>News</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleNavigate('Videos')}>
-                        <Text style={styles.navItem}>Videos</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => Alert.alert("This feature is coming soon!!")}>
-                        <Text style={styles.navItem}>Chat with our AI Nurse</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleScrollTo('about')}>
-                        <Text style={styles.navItem}>About</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleScrollTo('contact')}>
-                        <Text style={styles.navItem}>Contact Us</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={handleLanguageSwitch}>
-                        <Text style={styles.navItem}>Switch Language</Text>
-                    </TouchableOpacity>
-                </View>
+               <View style={styles.sideNav}>
+               <TouchableOpacity onPress={() => handleNavigate('home')}>
+                   <Text style={styles.navItem}>Home</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('Articles')}>
+                   <Text style={styles.navItem}>Articles</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('News')}>
+                   <Text style={styles.navItem}>News</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('Videos')}>
+                   <Text style={styles.navItem}>Videos</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('AIBot')}>
+                   <Text style={styles.navItem}>Chat with our AI Nurse</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('home', { scrollTo: 'about' })}>
+                   <Text style={styles.navItem}>About</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => handleNavigate('home', { scrollTo: 'contact' })}>
+                   <Text style={styles.navItem}>Contact Us</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={handleLanguageSwitch}>
+                   <Text style={styles.navItem}>Switch Language</Text>
+               </TouchableOpacity>
+           </View>
             )}
         </>
     );
@@ -48,10 +48,11 @@ const styles = StyleSheet.create({
     sideNav: {
         position: 'absolute',
         top: 0,
-        left: 0,
-        width: '70%',
+        right: 0,
+        width: '60%',
         height: '100%',
         backgroundColor: '#D21124',
+        paddingTop: 70,
         padding: 20,
         zIndex: 100,
     },
